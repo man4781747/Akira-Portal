1caa9e9816f175e86c1c078b10843145f50f6024
man4781747 <man4781747@gmail.com>
2024-07-23 21:52:07 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,51 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import postcssNesting from 'postcss-nesting';
 import path from 'path'
+import fs from 'fs'
 var MainHtmlPath = "index.html"
+
+// 自定义插件，用于打印目录下的文件路径
+function printFilesPlugin() {
+  return {
+    name: 'print-files-plugin',
+    buildStart() {
+      const dirPath = path.resolve(__dirname, 'src/assets')
+      printFiles(dirPath)
+    }
+  }
+}
+
+// 递归打印目录下的文件路径
+function printFiles(dirPath) {
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      console.error(`Could not list the directory: ${err}`)
+      process.exit(1)
+    }
+
+    files.forEach((file, index) => {
+      const filePath = path.join(dirPath, file)
+
+      fs.stat(filePath, (error, stat) => {
+        if (error) {
+          console.error(`Error stating file: ${error}`)
+          return
+        }
+
+        if (stat.isFile()) {
+          console.log(`File: ${filePath}`)
+        } else if (stat.isDirectory()) {
+          console.log(`Directory: ${filePath}`)
+          printFiles(filePath)
+        }
+      })
+    })
+  })
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), printFilesPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
